b324e920012532d392bb958b46c46fa87240bf75
kchang11230 <2773718162@qq.com>
2025-03-03 22:17:54 +0800

    Edit some files

--- index.js
+++ index.js
@@ -6,6 +6,20 @@
  * MIT Licensed
  */
 
-'use strict';
+// 'use strict';
 
-module.exports = require('./lib/express');
+// module.exports = require('./lib/express');
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, FunctionCat!");
+});
+
+// FunctionCat 需要监听 9000 端口
+const port = process.env.PORT || 9000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
